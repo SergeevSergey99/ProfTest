3f26b2a9c45dfcc54b2bf3185f83e02d038fd138
SergeevSergey99 <sergeev_sergey99@mail.ru>
2020-02-07 00:07:39 +0300

    Что то изменил

---
+++ build/precache-manifest.921de2f3e916b8ba39ae26092f454f3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b81a5f673ffaa6e812c7bf9e3f7f05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6db4dc26dc3e87b8a28e",
+    "url": "/static/css/main.91cbab03.chunk.css"
+  },
+  {
+    "revision": "136a912dae88d965265f",
+    "url": "/static/js/2.94cd062f.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.94cd062f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db4dc26dc3e87b8a28e",
+    "url": "/static/js/main.fe0f0098.chunk.js"
+  },
+  {
+    "revision": "42a8b147aa10dbde66ec",
+    "url": "/static/js/runtime-main.9f7c15aa.js"
+  }
+]);
