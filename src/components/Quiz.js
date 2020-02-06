70783dc6dd60219fa3e5f3c9597463ffd35aa177
SergeevSergey99 <sergeev_sergey99@mail.ru>
2020-02-06 23:33:22 +0300

    resulting

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -32,36 +32,36 @@ class Quiz extends React.Component {
     componentDidMount() {
         this.loadQuiz();
     }
-/*
-    nextQuestionHandlerVERY = () => {
+
+    /*
+        nextQuestionHandlerVERY = () => {
+            this.setState({
+                _Gum: this.state._Gum + this.state.Gum,
+                _Tech: this.state._Tech + this.state.Tech,
+                _Nat: this.state._Nat + this.state.Nat,
+                currentQuestion: this.state.currentQuestion + 1
+            });
+        };
+        nextQuestionHandlerMEDIUM = () => {
+            this.setState({
+                _Gum: this.state._Gum + this.state.Gum / 2,
+                _Tech: this.state._Tech + this.state.Tech / 2,
+                _Nat: this.state._Nat + this.state.Nat / 2,
+                currentQuestion: this.state.currentQuestion + 1
+            });
+        };
+        nextQuestionHandlerNOT = () => {
         this.setState({
-            _Gum: this.state._Gum + this.state.Gum,
-            _Tech: this.state._Tech + this.state.Tech,
-            _Nat: this.state._Nat + this.state.Nat,
-            currentQuestion: this.state.currentQuestion + 1
+          _Gum: this.state._Gum,
+          _Tech: this.state._Tech,
+          _Nat: this.state._Nat,
+          currentQuestion: this.state.currentQuestion + 1
         });
-    };
-    nextQuestionHandlerMEDIUM = () => {
-        this.setState({
-            _Gum: this.state._Gum + this.state.Gum / 2,
-            _Tech: this.state._Tech + this.state.Tech / 2,
-            _Nat: this.state._Nat + this.state.Nat / 2,
-            currentQuestion: this.state.currentQuestion + 1
-        });
-    };
-    nextQuestionHandlerNOT = () => {
-    this.setState({
-      _Gum: this.state._Gum,
-      _Tech: this.state._Tech,
-      _Nat: this.state._Nat,
-      currentQuestion: this.state.currentQuestion + 1
-    });
-  };
-*/
+      };
+    */
 
 
-
-  componentDidUpdate(prevProps, prevState) {
+    componentDidUpdate(prevProps, prevState) {
         const {currentQuestion} = this.state;
         if (this.state.currentQuestion !== prevState.currentQuestion) {
             if (this.state.currentQuestion <= QuizData.length - 1) {
@@ -78,9 +78,9 @@ class Quiz extends React.Component {
                 });
 
                 console.log(this.state.currentQuestion);
-               /* console.log(
-                    this.state._Hud + " " + this.state._Tech + " " + this.state._Nat
-                );*/
+                /* console.log(
+                     this.state._Hud + " " + this.state._Tech + " " + this.state._Nat
+                 );*/
             }
         }
     }
@@ -89,18 +89,18 @@ class Quiz extends React.Component {
         this.setState({rating: nextValue});
 
         this.setState({
-            _Soc: this.state._Soc + this.state.Soc * (nextValue-1),
-            _Tech: this.state._Tech + this.state.Tech * (nextValue-1),
-            _Hud: this.state._Hud + this.state.Hud * (nextValue-1),
-            _Num: this.state._Num + this.state.Num * (nextValue-1),
-            _Nat: this.state._Nat + this.state.Nat * (nextValue-1),
+            _Soc: this.state._Soc + this.state.Soc * (nextValue - 1),
+            _Tech: this.state._Tech + this.state.Tech * (nextValue - 1),
+            _Hud: this.state._Hud + this.state.Hud * (nextValue - 1),
+            _Num: this.state._Num + this.state.Num * (nextValue - 1),
+            _Nat: this.state._Nat + this.state.Nat * (nextValue - 1),
             currentQuestion: this.state.currentQuestion + 1
         });
 
     }
 
-  render() {
-      const { rating } = this.state;
+    render() {
+        const {rating} = this.state;
         if (this.state.currentQuestion <= QuizData.length - 1) {
             return (
                 <div>
@@ -115,12 +115,12 @@ class Quiz extends React.Component {
                     </div>
                     <div className="grade">
                         <div className="stars">
-                    <StarRatingComponent
-                        name="rate1"
-                        starCount={5}
-                        value={rating}
-                        onStarClick={this.onStarClick.bind(this)}
-                    />
+                            <StarRatingComponent
+                                name="rate1"
+                                starCount={5}
+                                value={rating}
+                                onStarClick={this.onStarClick.bind(this)}
+                            />
                         </div>
                     </div>
 
@@ -153,36 +153,31 @@ class Quiz extends React.Component {
                         </button>
             * */
         } else
-            return (
-                <div className="card">
-                    <p>Ваши результаты:</p>
-                    <table>
-                        <tbody>
-                        <tr>
-                            <td>C</td>
-                            <td>{this.state._Soc}</td>
-                        </tr>
-                        <tr>
-                            <td>Ц</td>
-                            <td>{this.state._Num}</td>
-                        </tr>
-                        <tr>
-                            <td>Х</td>
-                            <td>{this.state._Hud}</td>
-                        </tr>
-                        <tr>
-                            <td>Т</td>
-                            <td>{this.state._Tech}</td>
-                        </tr>
-                        <tr>
-                            <td>Е</td>
-                            <td>{this.state._Nat}</td>
-                        </tr>
-                        </tbody>
-                    </table>
+            var sum = this.state._Soc + this.state._Nat + this.state._Tech + this.state._Hud + this.state._Num;
+        var max = Math.max(this.state._Soc, this.state._Nat, this.state._Tech, this.state._Hud, this.state._Num);
 
+        return (
+            <div className="card_results">
+                <div className="result_text">Результаты профориентационного тестирования:</div>
+
+                <div className="results" style={{background: '#72D363', width: 300 + 200 * this.state._Num / max}}>
+                    Цифровое направление — {(this.state._Num / sum * 100).toFixed(1)} %
                 </div>
-            );
+                <div className="results" style={{background: '#6087D5', width: 300 + 200 * this.state._Soc / max}}>
+                    Социальное направление — {(this.state._Soc / sum * 100).toFixed(1)} %
+                </div>
+                <div className="results" style={{background: '#F1AA3F', width: 300 + 200 * this.state._Hud / max}}>
+                    Артистическое направление — {(this.state._Hud / sum * 100).toFixed(1)} %
+                </div>
+                <div className="results" style={{background: '#B962EF', width: 300 + 200 * this.state._Tech / max}}>
+                    Техническое направление — {(this.state._Tech / sum * 100).toFixed(1)} %
+                </div>
+                <div className="results" style={{background: '#EF88A7', width: 300 + 200 * this.state._Nat / max}}>
+                    Естественно-научное направление — {(this.state._Nat / sum * 100).toFixed(1)} %
+                </div>
+
+            </div>
+        );
     }
 }
 
