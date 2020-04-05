4ac47aaac44117343aad720a341ca1e7b043bbc8
SergeevSergey99 <sergeev_sergey99@mail.ru>
2020-04-05 16:40:32 +0300

    centring

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -7,16 +7,17 @@ class StartPage extends React.Component {
 
             return (
                 <div>
-
-
                     <div className="card_results">
-                        <div className="card-start_text" onClick={() => {document.location.href = "#/Registration";}}>Регистрация</div>
-
-                        <div className="card-start_text" onClick={() => {document.location.href = "#/Quiz";}}>Тест</div>
-                        <div className="card-start_text" onClick={() => {document.location.href = "#/Results";}}>Результаты</div>
-                        <div className="card-start_text" onClick={() => {document.location.href = "#/Rating";}}>Рейтинг</div>
-                        <div className="card-start_text" onClick={() => {document.location.href = "#/Events";}}>События</div>
-
+                        <div className="card-start_text" onClick={() => {document.location.href = "#/Registration";}}>
+                            <div className="inner">Регистрация</div></div>
+                        <div className="card-start_text" onClick={() => {document.location.href = "#/Quiz";}}>
+                            <div className="inner">Тест</div></div>
+                        <div className="card-start_text" onClick={() => {document.location.href = "#/Results";}}>
+                            <div className="inner">Результаты</div></div>
+                        <div className="card-start_text" onClick={() => {document.location.href = "#/Rating";}}>
+                            <div className="inner">Рейтинг</div></div>
+                        <div className="card-start_text" onClick={() => {document.location.href = "#/Events";}}>
+                            <div className="inner">События</div></div>
                     </div>
                 </div>
             );
